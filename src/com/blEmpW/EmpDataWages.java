9f8717c4f5ba4199af5996ba85e8491f4eee228f
anuroopkanna <anuroopindia16@gmail.com>
2023-03-03 02:15:24 +0530

    added:SwitchCase

--- src/com/blEmpW/EmpDataWages.java
+++ src/com/blEmpW/EmpDataWages.java
@@ -4,31 +4,35 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class EmpDataWages {
+
+
+        private static final int emp_FullTime=1;
+        private  static final int emp_PartTime=2;
+
+        private static final int emp_WagePerHour=20;
     public static void main(String[] args) {
-        System.out.println("Welcome to Bridgelabz EmpDataWages");
-        int emp_FullTime=1;
-        int emp_PartTime=2;
-        int emp_WagePerHour=20;
         int empHrs,empWage;
        // Scanner sc=new Scanner(System.in);
       //  System.out.println("Enter the values 2 or 1");
         //int emp=sc.nextInt();
-        double empCheck = Math.floor(Math.random()*10)%2;
-        if (empCheck==emp_FullTime){
+        int  empType  =(int) Math.floor(Math.random()*10)%3;
+        System.out.println("Welcome to Bridgelabz EmpDataWages");
+        switch (empType){
+            case emp_FullTime:
             System.out.println("Employee is FullTime Job Holder.");
             empHrs=8;
-        } else if (empCheck==emp_PartTime) {
+            break;
+        case emp_PartTime:
             System.out.println("Employee is PartTime Job Holder.");
             empHrs=4;
-        }else {
+            break;
+            default:
         System.out.println("Employee is absent");
             empHrs=0;
+            break;
         }
         empWage=empHrs*emp_WagePerHour;
         System.out.println("Emp Hours: " +empHrs);
         System.out.println("Emp Wages: "+empWage);
-
-
-
     }
 }
