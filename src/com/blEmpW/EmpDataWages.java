dfdf500af196185c5c9dd3f6855c99580f73e8d6
anuroopkanna <anuroopindia16@gmail.com>
2023-03-03 01:22:35 +0530

    added:EmpPA

--- src/com/blEmpW/EmpDataWages.java
+++ src/com/blEmpW/EmpDataWages.java
@@ -1,7 +1,23 @@
 package com.blEmpW;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class EmpDataWages {
     public static void main(String[] args) {
         System.out.println("Welcome to Bridgelabz EmpDataWages");
+        int empPresent;
+        System.out.println("Enter the values 0 or 1");
+        Scanner sc=new Scanner(System.in);
+         empPresent = sc.nextInt();
+        double empCheck = Math.floor(Math.random()*10)%2;
+        if (empCheck == empPresent){
+            System.out.println("Employee is present");
+        }else {
+            System.out.println("Employee is absent");
+        }
+
+
+
     }
 }
