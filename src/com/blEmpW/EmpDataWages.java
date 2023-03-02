9d02c6dcb41079fa14140af6e532b175cac1628c
anuroopkanna <anuroopindia16@gmail.com>
2023-03-03 02:33:23 +0530

    added:MonthlyWages

--- src/com/blEmpW/EmpDataWages.java
+++ src/com/blEmpW/EmpDataWages.java
@@ -8,15 +8,13 @@ public class EmpDataWages {
 
         private static final int emp_FullTime=1;
         private  static final int emp_PartTime=2;
-
         private static final int emp_WagePerHour=20;
+        private static final int emp_WorkingDaysperMonth=20;
     public static void main(String[] args) {
-        int empHrs,empWage;
-       // Scanner sc=new Scanner(System.in);
-      //  System.out.println("Enter the values 2 or 1");
-        //int emp=sc.nextInt();
+        System.out.println("Emp Working Days Per Month");
+        int empHrs=0,empWagePerDay=0,empWagePerMonth=0;
         int  empType  =(int) Math.floor(Math.random()*10)%3;
-        System.out.println("Welcome to Bridgelabz EmpDataWages");
+        System.out.println("Switch case starts");
         switch (empType){
             case emp_FullTime:
             System.out.println("Employee is FullTime Job Holder.");
@@ -31,8 +29,10 @@ public class EmpDataWages {
             empHrs=0;
             break;
         }
-        empWage=empHrs*emp_WagePerHour;
+        empWagePerDay=empHrs*emp_WagePerHour;
+        empWagePerMonth= empHrs*empWagePerDay;
         System.out.println("Emp Hours: " +empHrs);
-        System.out.println("Emp Wages: "+empWage);
+        System.out.println("Emp WagePerDay: "+empWagePerDay);
+        System.out.println("Emp WagesPerMonth: " +empWagePerMonth);
     }
 }
