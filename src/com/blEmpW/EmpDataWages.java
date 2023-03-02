08831bae1c07347759eef9bbcef1f182b61f3d34
anuroopkanna <anuroopindia16@gmail.com>
2023-03-03 02:00:44 +0530

    added:PartTime

--- src/com/blEmpW/EmpDataWages.java
+++ src/com/blEmpW/EmpDataWages.java
@@ -6,18 +6,22 @@ import java.util.Scanner;
 public class EmpDataWages {
     public static void main(String[] args) {
         System.out.println("Welcome to Bridgelabz EmpDataWages");
-        int emp_Present =1;
+        int emp_FullTime=1;
+        int emp_PartTime=2;
         int emp_WagePerHour=20;
         int empHrs,empWage;
-        System.out.println("Enter the values 0 or 1");
-        Scanner sc=new Scanner(System.in);
-         emp_Present = sc.nextInt();
+       // Scanner sc=new Scanner(System.in);
+      //  System.out.println("Enter the values 2 or 1");
+        //int emp=sc.nextInt();
         double empCheck = Math.floor(Math.random()*10)%2;
-        if (empCheck == emp_Present){
-            System.out.println("Employee is present");
+        if (empCheck==emp_FullTime){
+            System.out.println("Employee is FullTime Job Holder.");
             empHrs=8;
+        } else if (empCheck==emp_PartTime) {
+            System.out.println("Employee is PartTime Job Holder.");
+            empHrs=4;
         }else {
-            System.out.println("Employee is absent");
+        System.out.println("Employee is absent");
             empHrs=0;
         }
         empWage=empHrs*emp_WagePerHour;
