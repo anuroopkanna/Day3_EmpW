106d70df4de0b0500224d123602ec89638008fba
anuroopkanna <anuroopindia16@gmail.com>
2023-03-08 10:18:13 +0530

    added:Killinggap

--- src/com/blEmpW/EmpDataWages.java
+++ src/com/blEmpW/EmpDataWages.java
@@ -4,8 +4,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class EmpDataWages {
-
-
         private static final int emp_FullTime=1;
         private  static final int emp_PartTime=2;
         private static final int emp_WagePerHour=20;
