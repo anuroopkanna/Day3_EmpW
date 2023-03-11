af0b569b44f99a0c9153d8a3afdce94d0608008d
anuroopkanna <anuroopindia16@gmail.com>
2023-03-11 15:52:58 +0530

    added:updated

--- src/com/blEmpW/EmpDataWages.java
+++ src/com/blEmpW/EmpDataWages.java
@@ -1,22 +1,30 @@
 package com.blEmpW;
 
 public class EmpDataWages {
+   private static final int emp_FullTime=1;
+    private  static final int emp_PartTime=2;
+    private final String company;
+    private final int emp_WagePerHour;
+    private final int emp_TotalWorkingHrs;
+    private final int emp_WorkingDaysPerMonth;
 
 
-  /*      private static final int emp_FullTime=1;
-        private  static final int emp_PartTime=2;
-        private static final int emp_WagePerHour=20;
-        private static final int emp_WorkingDaysPerMonth=20;
-        private static final int emp_TotalWorkingHrs=100;*/
-    public static void CalulateTotalWages(String CompanyName,int emp_WagePerHour,int emp_TotalWorkingHrs,int emp_WorkingDaysPerMonth) {
+
+  public EmpDataWages(String company, int emp_WagePerHour, int emp_TotalWorkingHrs, int emp_WorkingDaysPerMonth){
+      this.company=company;
+      this.emp_WagePerHour= emp_WagePerHour;
+      this.emp_TotalWorkingHrs= emp_TotalWorkingHrs;
+      this.emp_WorkingDaysPerMonth= emp_WorkingDaysPerMonth;
+
+  }
+    public void CalulateTotalWages() {
         System.out.println("Welcome to the Employee Wages: ");
-        final int emp_FullTime=1;
-        final int emp_PartTime=2;
+
         int empHrs=0;
         int empWagePerMonth = 20;
         int empTotalHrs = 0;
         int empTotalWorkingDays = 0;
-        System.out.println("Details of "+CompanyName+"employee");
+        System.out.println("Details of " +company+ "employee");
         System.out.println("emp_WagePerHour "+emp_WagePerHour);
         System.out.println("Total working days "+empTotalWorkingDays);
         System.out.println("Total working hours "+empTotalHrs);
@@ -47,12 +55,23 @@ public class EmpDataWages {
         System.out.println("Emp WagePerDay: " + empWagePerDay);
         System.out.println("Emp WagesPerMonth: " + empWagePerMonth);
     }
+    public String toString(){
+      return super.toString();
+    }
 
     public static void main(String[] args) {
-       EmpDataWages e =new EmpDataWages();
-        //e.CalulateTotalWages();
-        e.CalulateTotalWages("Tesla",80,15,400);
-        e.CalulateTotalWages("TCS",100,10,1000);
-        e.CalulateTotalWages("Infosys",90,8,900);
+       EmpDataWages e =new EmpDataWages("Tesla",20,4,10);
+
+        EmpDataWages e1  = new EmpDataWages("Infosys",40,5,20);
+        EmpDataWages e2 = new EmpDataWages("Tata",45,6,20);
+         e.CalulateTotalWages();
+        System.out.println(e);
+        System.out.println();
+        e1.CalulateTotalWages();
+        System.out.println(e1);
+        System.out.println();
+        e2.CalulateTotalWages();
+        System.out.println(e2);
+        System.out.println();
     }
 }
